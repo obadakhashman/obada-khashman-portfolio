270e6a98957c16520cda72ce663f9eb5e95f7540
obadakhashman <obadakhashman10@gmail.com>
2026-09-18 02:07:08 +0300

    app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import KeyVentures from "@/components/KeyVentures";
+import Journey from "@/components/Journey";
+import Insights from "@/components/Insights";
+import Contact from "@/components/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <KeyVentures />
+      <Journey />
+      <Insights />
+      <Contact />
+    </>
+  );
+}
